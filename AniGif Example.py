994f3725d4bffb814e0c0772cad93b6dec642fc7
Lee Suddaby <leesuddaby@gmail.com>
2020-05-20 19:53:21 +0100

    Custom fps in gif

--- AniGif Example.py
+++ AniGif Example.py
@@ -4,16 +4,19 @@ from anigif import *
 pygame.init()
 clock = pygame.time.Clock()
 
-height = 360
-width = 936
-
-imagesCA = [" "] * 12
+imagesCA = [""] * 12
 for counter in range(12):
     imagesCA[counter] = "frames/coin_frame_" + str(counter+1) + ".png"
 
-coin = AnimatedGif(100,100,96,96,imagesCA) #Two coin animations at the bottom-left and bottom-right corners of the screen
+coin1 = AnimatedGif(25,100,96,96,imagesCA, 10)
+coin2 = AnimatedGif(140,100,96,96,imagesCA, 20)
+
+font_fps = pygame.font.SysFont('Arial', 24)
+font1 = font_fps.render("FPS = 10", True, (255,255,255))
+font2 = font_fps.render("FPS = 20", True, (255,255,255))
 
 screen = pygame.display.set_mode([250,250])
+pygame.display.set_caption("AniGif Example")
 screen.fill((0,0,0))
 running = True
 
@@ -23,8 +26,12 @@ while running:
             running = False
 
     screen.fill((0,0,0))
-    screen.blit(coin.getCurFrame(), [coin.gif_x,coin.gif_y])
-    clock.tick(10)
+    screen.blit(coin1.getCurFrame(), [coin1.x,coin1.y])
+    screen.blit(coin2.getCurFrame(), [coin2.x,coin2.y])
+    screen.blit(font1, [25, 200])
+    screen.blit(font2, [140, 200])
+
+    clock.tick(25)
     pygame.display.flip() #Update display
 
 pygame.quit()
--- anigif/anigif.py
+++ anigif/anigif.py
@@ -1,27 +1,26 @@
 import numpy as np
 import pygame
 
-#------------------------------AnimatedGif Class------------------------------
 #Custom object containing data for displaying an animated GIF
 #Created as python is not very friendly when it comes to interacting with animated GIFs
 class AnimatedGif:
-    #Constructor - Should be relatively self-explanatory. new_paths is an array of file paths as strings
-    def __init__(self, new_x, new_y, new_w, new_h, new_paths):
+    def __init__(self, new_x, new_y, new_w, new_h, new_paths, new_fps=10):
         self.frameCounter = 0 #Index of frame array that contains the next image to be displayed
-        self.gif_x = new_x
-        self.gif_y = new_y
-        self.gif_w = new_w
-        self.gif_h = new_h
+        self.x = new_x
+        self.y = new_y
+        self.w = new_w
+        self.h = new_h
+        self.fps = new_fps #FPS = Frames per Second
+        self.time_dif = int(1000/self.fps)
+        self.init_ticks = pygame.time.get_ticks()
         self.noOfFrames = len(new_paths)
         self.framePaths = np.array(new_paths) #Array of string, to store the file paths
         self.frames = np.array([None] * self.noOfFrames) #Array of pygame Surface objects, to store the actual graphical frames
         for n in range(self.noOfFrames):
-            self.frames[n] = pygame.transform.scale(pygame.image.load(new_paths[n]), [self.gif_w, self.gif_h])
+            self.frames[n] = pygame.transform.scale(pygame.image.load(new_paths[n]), [self.w, self.h])
 
-    #Return next frame of the GIF to be displayed
-    #Set to loop the GIF indefinitely
+    #Return frame of the GIF to be displayed at any given moment, based on ticks (i.e. time) and fps of the gif
+    #The GIF loops indefinitely
     def getCurFrame(self):
-        self.frameCounter = self.frameCounter + 1
-        if self.frameCounter  >= self.noOfFrames:
-            self.frameCounter = 0 #Play GIF from beginning as frame cycle completed
+        self.frameCounter = int((pygame.time.get_ticks() - self.init_ticks) / self.time_dif) % self.noOfFrames
         return self.frames[self.frameCounter]
